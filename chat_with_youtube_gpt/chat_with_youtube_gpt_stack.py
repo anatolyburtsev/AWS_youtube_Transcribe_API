0c05c24e0e1609d84ff76decc08713af07085368
Anatolii Burtsev <ab.canada.world@gmail.com>
2023-11-19 15:56:54 -0800

    initial commit. Simple lambda behind API GW with pass

--- chat_with_youtube_gpt/chat_with_youtube_gpt_stack.py
+++ chat_with_youtube_gpt/chat_with_youtube_gpt_stack.py
@@ -1,19 +1,72 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as lambda_,
+
+    aws_apigateway as apigateway,
+    CfnOutput, Duration
 )
 from constructs import Construct
+
 
 class ChatWithYoutubeGptStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Define the Lambda function
+        transcribe_lambda = lambda_.Function(
+            self, 'TranscribeHandler',
+            function_name='TranscribeHandler',
+            runtime=lambda_.Runtime.FROM_IMAGE,
+            code=lambda_.Code.from_asset_image('lambda'),
+            handler=lambda_.Handler.FROM_IMAGE,
+            timeout=Duration.minutes(5)
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ChatWithYoutubeGptQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the API Gateway
+        api = apigateway.RestApi(
+            self, 'Endpoint',
+            rest_api_name='YoutubeTranscribeAPI'
+        )
+
+        # Lambda Integration
+        transcribe_integration = apigateway.LambdaIntegration(transcribe_lambda)
+
+        # Define API Key for the POST method
+        api_key = apigateway.ApiKey(
+            self, 'TranscribeApiKey',
+            enabled=True
+        )
+
+        # Define Usage Plan
+        usage_plan = apigateway.UsagePlan(
+            self, 'UsagePlan',
+            name='BasicUsagePlan',
+            throttle={
+                'rate_limit': 10,
+                'burst_limit': 2
+            }
+        )
+
+        usage_plan.add_api_key(api_key)
+        usage_plan.add_api_stage(
+            stage=api.deployment_stage
+        )
+
+        transcribe_resource = api.root.add_resource('transcribe')
+        transcribe_resource.add_method(
+            'POST', transcribe_integration,
+            api_key_required=True
+        )
+
+        CfnOutput(
+            self, 'ApiUrl',
+            value=api.url,
+            description='The URL of the API Gateway'
+        )
+
+        CfnOutput(
+            self, 'ApiKey',
+            value=api_key.key_id,
+            description='API Key Id for accessing the POST method'
+        )
---
+++ test_request.sh
@@ -0,0 +1,4 @@
+curl -X POST https://XXXXXXX.execute-api.us-east-1.amazonaws.com/prod/order/ \
+-H "Content-Type: application/json" \
+-H "x-api-key: KEY_IS_HERE" \
+-d '{"client_name":"John Doe"}'
